a03e89824a10c6d9e33c443e0dd56c73103c383e
Benoit Bovy <bbovy@ulg.ac.be>
2015-11-18 01:14:22 +0100

    added packages + tmux compatibility
    (added editorconfig, multi-term, package-utils, ranger)

--- myconf.el
+++ myconf.el
@@ -18,6 +18,10 @@
                                  sr-speedbar
                                  nginx-mode
                                  linum-off
+                                 editorconfig
+                                 multi-term
+                                 package-utils
+                                 ranger
                                  ) prelude-packages))
 (prelude-install-packages)
 
@@ -46,16 +50,16 @@
 
 
 ;; -- line numbers
-;;(require 'linum-off)   ;; v0.1 bug, doesn't load
-;;(global-linum-mode 1)
+(require 'linum-off)   ;; v0.1 bug, doesn't load
+(global-linum-mode 1)
 
 (global-set-key (kbd "C-x n") 'linum-mode)
 
 (unless window-system
   (add-hook 'linum-before-numbering-hook
-             (lambda ()
+            (lambda ()
               (setq-local linum-format-fmt
-                            (let ((w (length (number-to-string
+                          (let ((w (length (number-to-string
                                             (count-lines (point-min) (point-max))))))
                             (concat "%" (number-to-string w) "d"))))))
 
@@ -65,7 +69,7 @@
    (propertize " " 'face 'mode-line)))
 
 (unless window-system
-    (setq linum-format 'linum-format-func))
+  (setq linum-format 'linum-format-func))
 
 (custom-set-faces '(linum ((t (:foreground "#6F6F6F" :background "#3F3F3F" :box nil)))))
 
@@ -98,10 +102,10 @@
   '(progn
      (setq speedbar-hide-button-brackets-flag t
            speedbar-show-unknown-files t
-            speedbar-smart-directory-expand-flag t
+           speedbar-smart-directory-expand-flag t
            speedbar-directory-button-trim-method 'trim
            speedbar-use-images nil
-            speedbar-indentation-width 2
+           speedbar-indentation-width 2
            speedbar-use-imenu-flag t
            speedbar-file-unshown-regexp "flycheck-.*"
            sr-speedbar-width 40
@@ -114,9 +118,9 @@
                '(lambda ()
                   (define-key speedbar-mode-map [C-S-up] 'speedbar-up-directory)
                   (define-key speedbar-mode-map [right] 'speedbar-flush-expand-line)
-                   (define-key speedbar-mode-map [left] 'speedbar-contract-line)))
+                  (define-key speedbar-mode-map [left] 'speedbar-contract-line)))
      )
-    )
+  )
 
 
 ;;  -- flycheck settings
@@ -144,7 +148,7 @@
   nil)
 
 (defun pc:magit-status-buffer-switch (buf)
-    "replacement for `magit-status-buffer-switch-function'.
+  "replacement for `magit-status-buffer-switch-function'.
 `magit-status' does not split windows (switch to magit buffer
 in stead). Also store the current buffer to switch back to it when
  quitting.
@@ -169,6 +173,99 @@ With a prefix argument, kill the magit buffer instead."
   (define-key magit-mode-map (kbd "q") 'pc:magit-quit-window)
   )
 (add-hook 'magit-mode-hook 'magit-mode-keys)
+
+
+;; handle tmux's xterm-keys
+;; put the following line in your ~/.tmux.conf:
+;;   setw -g xterm-keys on
+(if (getenv "TMUX")
+    (progn
+      (let ((x 2) (tkey ""))
+        (while (<= x 8)
+          ;; shift
+          (if (= x 2)
+              (setq tkey "S-"))
+          ;; alt
+          (if (= x 3)
+              (setq tkey "M-"))
+          ;; alt + shift
+          (if (= x 4)
+              (setq tkey "M-S-"))
+          ;; ctrl
+          (if (= x 5)
+              (setq tkey "C-"))
+          ;; ctrl + shift
+          (if (= x 6)
+              (setq tkey "C-S-"))
+          ;; ctrl + alt
+          (if (= x 7)
+              (setq tkey "C-M-"))
+          ;; ctrl + alt + shift
+          (if (= x 8)
+              (setq tkey "C-M-S-"))
+
+          ;; arrows
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d A" x)) (kbd (format "%s<up>" tkey)))
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d B" x)) (kbd (format "%s<down>" tkey)))
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d C" x)) (kbd (format "%s<right>" tkey)))
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d D" x)) (kbd (format "%s<left>" tkey)))
+          ;; home
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d H" x)) (kbd (format "%s<home>" tkey)))
+          ;; end
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d F" x)) (kbd (format "%s<end>" tkey)))
+          ;; page up
+          (define-key key-translation-map (kbd (format "M-[ 5 ; %d ~" x)) (kbd (format "%s<prior>" tkey)))
+          ;; page down
+          (define-key key-translation-map (kbd (format "M-[ 6 ; %d ~" x)) (kbd (format "%s<next>" tkey)))
+          ;; insert
+          (define-key key-translation-map (kbd (format "M-[ 2 ; %d ~" x)) (kbd (format "%s<delete>" tkey)))
+          ;; delete
+          (define-key key-translation-map (kbd (format "M-[ 3 ; %d ~" x)) (kbd (format "%s<delete>" tkey)))
+          ;; f1
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d P" x)) (kbd (format "%s<f1>" tkey)))
+          ;; f2
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d Q" x)) (kbd (format "%s<f2>" tkey)))
+          ;; f3
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d R" x)) (kbd (format "%s<f3>" tkey)))
+          ;; f4
+          (define-key key-translation-map (kbd (format "M-[ 1 ; %d S" x)) (kbd (format "%s<f4>" tkey)))
+          ;; f5
+          (define-key key-translation-map (kbd (format "M-[ 15 ; %d ~" x)) (kbd (format "%s<f5>" tkey)))
+          ;; f6
+          (define-key key-translation-map (kbd (format "M-[ 17 ; %d ~" x)) (kbd (format "%s<f6>" tkey)))
+          ;; f7
+          (define-key key-translation-map (kbd (format "M-[ 18 ; %d ~" x)) (kbd (format "%s<f7>" tkey)))
+          ;; f8
+          (define-key key-translation-map (kbd (format "M-[ 19 ; %d ~" x)) (kbd (format "%s<f8>" tkey)))
+          ;; f9
+          (define-key key-translation-map (kbd (format "M-[ 20 ; %d ~" x)) (kbd (format "%s<f9>" tkey)))
+          ;; f10
+          (define-key key-translation-map (kbd (format "M-[ 21 ; %d ~" x)) (kbd (format "%s<f10>" tkey)))
+          ;; f11
+          (define-key key-translation-map (kbd (format "M-[ 23 ; %d ~" x)) (kbd (format "%s<f11>" tkey)))
+          ;; f12
+          (define-key key-translation-map (kbd (format "M-[ 24 ; %d ~" x)) (kbd (format "%s<f12>" tkey)))
+          ;; f13
+          (define-key key-translation-map (kbd (format "M-[ 25 ; %d ~" x)) (kbd (format "%s<f13>" tkey)))
+          ;; f14
+          (define-key key-translation-map (kbd (format "M-[ 26 ; %d ~" x)) (kbd (format "%s<f14>" tkey)))
+          ;; f15
+          (define-key key-translation-map (kbd (format "M-[ 28 ; %d ~" x)) (kbd (format "%s<f15>" tkey)))
+          ;; f16
+          (define-key key-translation-map (kbd (format "M-[ 29 ; %d ~" x)) (kbd (format "%s<f16>" tkey)))
+          ;; f17
+          (define-key key-translation-map (kbd (format "M-[ 31 ; %d ~" x)) (kbd (format "%s<f17>" tkey)))
+          ;; f18
+          (define-key key-translation-map (kbd (format "M-[ 32 ; %d ~" x)) (kbd (format "%s<f18>" tkey)))
+          ;; f19
+          (define-key key-translation-map (kbd (format "M-[ 33 ; %d ~" x)) (kbd (format "%s<f19>" tkey)))
+          ;; f20
+          (define-key key-translation-map (kbd (format "M-[ 34 ; %d ~" x)) (kbd (format "%s<f20>" tkey)))
+
+          (setq x (+ x 1))
+          ))
+      )
+  )
 
 
 (provide 'myconf)
