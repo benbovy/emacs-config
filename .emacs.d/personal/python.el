15ff9038b0c1d97c0197a2f0236ed721cbdd6949
Benoit Bovy <benbovy@gmail.com>
2018-02-26 09:47:53 +0100

    use-package for python configuration

--- .emacs.d/personal/python.el
+++ .emacs.d/personal/python.el
@@ -8,23 +8,30 @@
 
 ;;; Code:
 
+(require 'use-package)
+
+
 ;; -- Elpy
-;; (prelude-require-package 'elpy)
-;; (require 'elpy)
-;; (elpy-enable)
-;; disable anaconda-mode
-;; (add-hook 'elpy-mode-hook (lambda () (anaconda-mode 0)))
+(prelude-require-package 'elpy)
 
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "-i --simple-prompt")
+(use-package elpy
+  :disabled
+  :init
+  (add-hook 'elpy-mode-hook (lambda () (anaconda-mode 0)))
+  :config
+  (elpy-enable)
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "-i --simple-prompt"))
 
-;; disable flymake used by elpy (we use flycheck instead)
-;; (when (require 'flycheck nil t)
-;;    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)))
+(use-package elpy
+  :disabled
+  :requires flycheck
+  :config
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)))
 
 
 ;; anaconda-mode doc
-(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+(add-hook 'python-mode 'anaconda-eldoc-mode)
 
 
 ;; disable which-function in Python mode
@@ -33,26 +40,29 @@
 
 
 ;; -- Conda
-;; Trick to select conda environment within emacs
-;; (see http://emacs.stackexchange.com/questions/20092/using-conda-environments-in-emacs)
-;; emacs must be run from a conda env activated!!
-;; TODO: by default select conda base environment if none is activated?
 (prelude-require-package 'pyvenv)
-(require 'pyvenv)
 
 (defvar conda-env-dir)
 
-(if (getenv "CONDA_PREFIX")
-    (progn
-     (setq conda-env-dir (file-name-directory (directory-file-name (getenv "CONDA_PREFIX"))))
-     (message "Conda environments directory: %s" conda-env-dir)
-     (setenv "WORKON_HOME" conda-env-dir)
-     (pyvenv-mode 1)
-     (defalias 'conda-workon 'pyvenv-workon)
-     (pyvenv-activate (getenv "CONDA_PREFIX"))
-     (message "Conda environment %s is activated" (getenv "CONDA_PREFIX"))
+(use-package pyvenv
+  :config
+  ;; Trick to select conda environment within emacs
+  ;; (see http://emacs.stackexchange.com/questions/20092/using-conda-environments-in-emacs)
+  ;; emacs must be run from a conda env activated!!
+  ;; TODO: by default select conda base environment if none is activated?
+  ;; TODO: doesn't work with base environment
+  (if (getenv "CONDA_PREFIX")
+      (progn
+        (setq conda-env-dir (file-name-directory (directory-file-name (getenv "CONDA_PREFIX"))))
+        (message "Conda environments directory: %s" conda-env-dir)
+        (setenv "WORKON_HOME" conda-env-dir)
+        (pyvenv-mode 1)
+        (defalias 'conda-workon 'pyvenv-workon)
+        (pyvenv-activate (getenv "CONDA_PREFIX"))
+        (message "Conda environment %s is activated" (getenv "CONDA_PREFIX"))
+        )
     )
-)
+  )
 
 
 (provide 'python)
